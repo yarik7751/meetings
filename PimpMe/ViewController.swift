0b6bb45dbf2aec7b10aababd4db6b5eaabdff6af
IgorMaisiuk <igor.maisiuk@elatesoftware.com>
2017-04-28 10:42:52 +0300

    Initial Commit

---
+++ PimpMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PimpMe
+//
+//  Created by Игорь Майсюк on 28.04.17.
+//  Copyright © 2017 ElateSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
