ec92991725b92e7aa1ee793b39dd81d8fd1477f1
Yaroslav <yaroslav.levshunov2@gmail.com>
2017-05-17 18:25:49 +0300

    #680 Превью новой встречи(Man) создание UI

--- app/src/main/java/com/elatesoftware/meetings/util/Const.java
+++ app/src/main/java/com/elatesoftware/meetings/util/Const.java
@@ -5,8 +5,11 @@ public class Const {
     public static final String RESPONSE = "response";
     public static final String SHARED_PREFERENCES_TITLE = "sp_currency_by_yarik";
     public static final String SP_IS_MAN = "SP_IS_MAN";
+    public static final String SP_IS_FIRST = "SP_IS_FIRST";
     public static final String SP_ID = "SP_ID";
     public static final String SP_WOMAN_INFORMATION = "SP_WOMAN_INFORMATION";
     public static final String SP_MAN_INFORMATION = "SP_MAN_INFORMATION";
+    public static final int MAN_VALUE = 1;
+    public static final int WOMAN_VALUE = 2;
     public static final int CODE_SUCCESS = 200;
 }
