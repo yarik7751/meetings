714bb2dcf81476969ff2826cbaeaf28e4c237f1c
Yaroslav <yaroslav.levshunov2@gmail.com>
2017-06-06 18:26:01 +0300

    #777 Экран Настроек(Man) дизайн

---
+++ app/src/main/java/com/elatesoftware/meetings/ui/fragment/SettingsFragment.java
@@ -0,0 +1,43 @@
+package com.elatesoftware.meetings.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.elatesoftware.meetings.R;
+import com.elatesoftware.meetings.ui.fragment.base.BaseFragment;
+
+public class SettingsFragment extends BaseFragment {
+
+    private static SettingsFragment fragment;
+    public static SettingsFragment getInstance() {
+        if(fragment == null) {
+            fragment = new SettingsFragment();
+        }
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_settings, null);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
